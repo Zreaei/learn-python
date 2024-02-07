be7500d5009417ef25637f091c516be1ec7264e7
Zreaei <zenarthxdgt@gmail.com>
2024-02-07 13:26:38 +0700

    testing

--- Self-Learn/main.cpp
+++ Self-Learn/main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 // Deklarasi Function diatas karena C++ membaca code dari atas
 int angka();
 int input();
+int dtype();
 
 // Main
 int main()
@@ -14,11 +15,14 @@ int main()
     std::cout << "Ini bukan baris baru. ";
     std::cout << "Ini akhir kalimat." << std::endl;
     
-    // Memanggil Function angka()
+    // Memanggil function angka()
     angka();
 
     // Memanggil function input()
     input();
+
+    // Memanggil function dtype()
+    dtype();
 
     std::cin.get();
     return 0;
@@ -48,6 +52,30 @@ int input()
     cin >> input;
     cout << "Angka yang anda masukan adalah: ";
     cout << input << endl;
+
+    cin.get();
+    return 0;
+}
+
+// Tipe data fundamental
+int dtype()
+{
+    // Bilangan bulat
+    short a = 2; // 16-bit/2 byte
+    int b = 4; // 32-bit/4 byte
+    long c = 6; // 64-bit/8 byte
+
+    // Bilangan desimal
+    float d = 1.0;
+    double e = 2.5;
+
+    // Character
+    char f = 'a'; //1-bit
+
+    // Boolean
+    bool g = true; // true/false
+
+    cout << a << endl;
 
     cin.get();
     return 0;
---
+++ Self-Learn/test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+    string nama;
+    cout << "Selamat datang!" << endl;
+    cout << "Masukan nama anda: ";
+    cin >> nama;
+    cout << "Nama anda adalah: ";
+    cout << nama << endl; 
+}
